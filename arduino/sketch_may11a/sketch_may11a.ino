038de20ac99c388bfe02653520086acc8cfbe3dd
Phillip Samuel K <phillipsamuelk3@gmail.com>
2022-05-11 23:09:34 +0300

    club meeting sketch

---
+++ arduino/sketch_may11a/sketch_may11a.ino
@@ -0,0 +1,22 @@
+// make variables
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // setup servo
+
+}
+
+void loop() {
+  // if someone is near
+  // open
+  // else: stay closed
+ //if not full, light green
+ //open
+ //else:stay closed,light red
+  
+
+}
+void measure(){
+  //
+}
